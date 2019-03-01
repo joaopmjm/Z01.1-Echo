5f36bf07a19aa1e58ed9ae1910ef894288f9a3a4
lucaslealvale <lucaslealvale01@gmail.com>
2019-03-01 18:48:23 -0300

    testado mux8way16

--- Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
@@ -19,4 +19,27 @@ architecture arch of Mux8Way16 is
 begin
 
 
+process(a,b,c,d,e,f,g,h,sel)
+	begin
+
+	if (sel = "000") then
+		q <= a;
+	elsif (sel = "001") then
+		q <= b;
+	elsif (sel = "010") then
+		q <= c;
+	elsif (sel = "011") then
+		q <= d;
+	elsif (sel = "100") then
+		q <= e;
+	elsif (sel = "101") then
+		q <= f;
+	elsif (sel = "110") then
+		q <= g;
+	elsif (sel = "111") then
+		q <= h;
+
+	end if;
+end process;
+
 end architecture;
